8276f228ad0a533cf29e675f555dd81a44bc27bf
juictonb18a1 <69259999+juictonb18a1@users.noreply.github.com>
2026-05-19 20:42:48 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,63 @@
+const cacheName = "cornhole-scoreboard-v11";
+const appShell = [
+  "./",
+  "./index.html",
+  "./styles.css?v=11",
+  "./app.js?v=11",
+  "./manifest.webmanifest",
+  "./icons/apple-touch-icon.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(appShell);
+    }),
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)));
+    }),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const responseCopy = response.clone();
+          caches.open(cacheName).then((cache) => {
+            cache.put("./index.html", responseCopy);
+          });
+          return response;
+        })
+        .catch(() => caches.match("./index.html")),
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(event.request).then((response) => {
+          const responseCopy = response.clone();
+          caches.open(cacheName).then((cache) => {
+            cache.put(event.request, responseCopy);
+          });
+          return response;
+        })
+      );
+    }),
+  );
+});
